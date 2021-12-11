8b03ce7576c33b26f8985be0cf7b4db2ff851431
lincoln auster [they/them] <lincolnauster@gmail.com>
2021-12-10 17:22:07 -0700

    add derivation output to flake
    `nix build` will now actually do the thing and build the binary. This
    derivation is just stolen from https://www.srid.ca/haskell-nix.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,13 @@
   outputs = { self, nixpkgs }: let
     pkgs = import nixpkgs { system = "x86_64-linux"; };
   in {
+    defaultPackage.x86_64-linux = pkgs.haskellPackages.developPackage {
+      root = ./.;
+      modifier = drv:
+        pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+          [ cabal-install ghcid ]);
+    };
+
     devShell.x86_64-linux = pkgs.mkShell {
       buildInputs = with pkgs; [ ghc cabal-install hlint ];
     };

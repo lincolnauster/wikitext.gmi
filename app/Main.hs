535648e24eeca05cd36450848941f45b0280d1af
lincoln auster [they/them] <lincolnauster@gmail.com>
2021-12-11 17:22:28 -0700

    add a usage message
    This is displayed with --help.

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,30 @@ module Main where
 
 import System.IO
 import System.Environment
+import Text.Printf
+
 import Parser
 
+data Action = Help | Convert String
+
 -- Given CLI args, locate and return a lazy string of the requested input.
-targetStream :: IO String
-targetStream = do a <- getArgs
-                  case a of
-                       ([])   -> getContents
-                       (x:[]) -> readFile x
+getTargetAction :: IO Action
+getTargetAction = do a <- getArgs
+                     case a of
+                          ("--help":[]) -> return Help
+                          ([])   -> getContents >>= \c -> return $ Convert c
+                          (f:[]) -> readFile f  >>= \c -> return $ Convert c
+
+performAction :: Action -> IO ()
+performAction Help = do
+  pname <- getProgName
+  putStrLn $ printf "Usage:\n\
+\  %s [--help] | <input-file>\n\
+\\n\
+\  An input file may be supplied in the first CLI argument. If none is present,\n\
+\  stdin is used." pname
+
+performAction (Convert s) = putStrLn $ gemtext s
 
 main :: IO ()
-main = targetStream >>= \c -> putStrLn $ gemtext c
+main = getTargetAction >>= performAction
